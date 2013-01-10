00599ae2393644f6f2fa39879f43c511a70d6a97
Ian Reah <ian.reah@gmail.com>
2013-01-10 09:26:10 +0000

    First

---
+++ KilnReviews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KilnReviews.Global" Language="C#" %>
---
+++ KilnReviews/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace KilnReviews
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			RouteTable.Routes.MapHttpRoute(name: "API Default",
+										   routeTemplate: "api/{controller}/{id}",
+										   defaults: new { id = RouteParameter.Optional });
+		}
+	}
+}
