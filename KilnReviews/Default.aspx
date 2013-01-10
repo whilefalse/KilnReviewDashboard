47da1c70744c78fd6abef0ecd6fbef1c0e65a57f
Ian Reah <ian.reah@gmail.com>
2013-01-10 09:41:47 +0000

    Use the api to get the reviews in the client

--- KilnReviews/Default.aspx
+++ KilnReviews/Default.aspx
@@ -7,5 +7,10 @@
     <title></title>
 </head>
 <body>
+	
+	<script type="text/javascript" src="Scripts/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="Scripts/spine/spine.js"></script>
+
+	<script type="text/javascript" src="main.js"></script>
 </body>
 </html>
---
+++ KilnReviews/main.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+	var ReviewsController = Spine.Controller.sub({
+		elements: {},
+		events: {},
+
+		init: function () {
+			$.getJSON("/api/Reviews", function(data) {
+				var reviews = $.parseJSON(data);
+				console.log(reviews);
+			});
+		}
+	});
+	
+	new ReviewsController({ el: $("body") });
+});
